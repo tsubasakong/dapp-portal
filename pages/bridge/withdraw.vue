e4adadbeb5fd4c430fc262e5579dba2006d9a493
Jack Hamer <jackhamer0990@gmail.com>
2024-05-06 13:01:54 +0300

    fix: temporarily disable withdrawals on testnet

--- pages/bridge/withdraw.vue
+++ pages/bridge/withdraw.vue
@@ -1,15 +1,28 @@
 <template>
   <ModalTransactionWithdrawalUnavailable />
 
-  <TransferView type="withdrawal">
-    <template #title>
-      <PageTitle>Bridge</PageTitle>
-    </template>
-  </TransferView>
+  <div v-if="withdrawalsDisabled">
+    <PageTitle>Bridge</PageTitle>
+    <CommonAlert variant="warning" :icon="ExclamationTriangleIcon" class="mb-block-gap">
+      <p>
+        {{ eraNetwork.name }} is going through an upgrade and withdrawals are disabled until it is finished (expected
+        May 6th 18h00 UTC). Please come back later.
+      </p>
+    </CommonAlert>
+
+    <div class="mt-5 flex flex-wrap items-center justify-center gap-block-gap">
+      <CommonButton as="RouterLink" :to="{ name: 'assets' }" size="xs">Go to Assets page</CommonButton>
+      <CommonButton size="xs" as="RouterLink" :to="{ name: 'bridge' }">Bridge to {{ eraNetwork.name }}</CommonButton>
+    </div>
+  </div>
+  <TransferView v-else type="withdrawal" />
 </template>
 
 <script lang="ts" setup>
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+
 import { bridge as bridgeMeta } from "@/data/meta";
+import { useZkSyncProviderStore } from "@/store/zksync/provider";
 import TransferView from "@/views/transactions/Transfer.vue";
 
 useSeoMeta({
@@ -23,6 +36,9 @@ useSeoMeta({
   twitterImage: bridgeMeta.previewImg.src,
   twitterImageAlt: bridgeMeta.previewImg.alt,
 });
+
+const { eraNetwork } = storeToRefs(useZkSyncProviderStore());
+const withdrawalsDisabled = computed(() => eraNetwork.value.key === "sepolia");
 </script>
 
 <style lang="scss" scoped></style>
